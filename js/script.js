735e7f7e9c752b7542b523059ff005d5abc3afeb
n48331 <unfolks@gmail.com>
2022-08-22 23:22:37 +0530

    track

--- js/script.js
+++ js/script.js
@@ -1,16 +1,56 @@
 $(document).ready(function () {
-  $("input").keyup(function () {
-    var datas = this.value;
+  $("button").click(function () {
+    var display =
+      "<tr><th>Distance</th><th>Time taken</th><th>time for 1000</th></tr>";
+    var datas = $("input").val();
     var time = 0;
     var dis_time;
     var data = datas.split(",");
-    $("h1").text(this.value);
 
     for (var i = 0; i < data.length; i++) {
       dis_time = data[i].split("-");
-      time += (dis_time[1] / dis_time[0]) * 1000;
+      var x = (dis_time[1] / dis_time[0]) * 1000;
+      time += x;
+      display +=
+        "<tr><td>" +
+        dis_time[0] +
+        "</td>" +
+        "<td>" +
+        dis_time[1] +
+        "</td>" +
+        "<td>" +
+        x +
+        "</td>" +
+        "</tr>";
     }
     var avg_time = time / data.length;
-    $("h2").text(avg_time);
+    $("table").html(display);
+    if (isNaN(avg_time)) {
+      $("h2").text("Enter value properly");
+    } else {
+      $("h2").text("Average time : " + avg_time.toFixed(2) + " sec");
+    }
   });
 });
+// $(document).ready(function () {
+//   var display = "";
+//   $("input").keyup(function () {
+//     var datas = this.value;
+//     var time = 0;
+//     var dis_time;
+//     var data = datas.split(",");
+
+//     for (var i = 0; i < data.length; i++) {
+//       dis_time = data[i].split("-");
+//       time += (dis_time[1] / dis_time[0]) * 1000;
+//     }
+//     // display += "<p>" + data[i] + "</p>";
+//     var avg_time = time / data.length;
+//     $("h1").html(display);
+//     if (isNaN(avg_time)) {
+//       $("h2").text("Enter value properly");
+//     } else {
+//       $("h2").text(avg_time);
+//     }
+//   });
+// });
